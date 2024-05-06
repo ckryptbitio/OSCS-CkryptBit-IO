b7c29a264b404d246ff35811309d8e5ac56da56a
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:45 +0200

    improve

--- back/bower_components/jquery-ui/ui/i18n/datepicker-ru.js
+++ back/bower_components/jquery-ui/ui/i18n/datepicker-ru.js
@@ -1,37 +1,78 @@
 /* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
 /* Written by Andrew Stromnov (stromnov@gmail.com). */
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
 
-		// AMD. Register as an anonymous module.
-		define([ "../datepicker" ], factory );
-	} else {
+(function( $ ) {
+  'use strict';
 
-		// Browser globals
-		factory( jQuery.datepicker );
-	}
-}(function( datepicker ) {
+  // If AMD is defined, use it; otherwise, assume a browser environment
+  if ( typeof define === 'function' && define.amd ) {
+    define([ 'jquery-ui/datepicker' ], factory);
+  } else {
+    factory($.datepicker);
+  }
 
-datepicker.regional['ru'] = {
-	closeText: 'Закрыть',
-	prevText: '&#x3C;Пред',
-	nextText: 'След&#x3E;',
-	currentText: 'Сегодня',
-	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-	'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
-	'Июл','Авг','Сен','Окт','Ноя','Дек'],
-	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-	weekHeader: 'Нед',
-	dateFormat: 'dd.mm.yy',
-	firstDay: 1,
-	isRTL: false,
-	showMonthAfterYear: false,
-	yearSuffix: ''};
-datepicker.setDefaults(datepicker.regional['ru']);
+  // The factory function for creating the regional settings
+  function factory(datepicker) {
+    const regionalOptions = {
+      closeText: 'Закрыть',
+      prevText: '&#x3C;Пред',
+      nextText: 'След&#x3E;',
+      currentText: 'Сегодня',
+      monthNames: [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь'
+      ],
+      monthNamesShort: [
+        'Янв',
+        'Фев',
+        'Мар',
+        'Апр',
+        'Май',
+        'Июн',
+        'Июл',
+        'Авг',
+        'Сен',
+        'Окт',
+        'Ноя',
+        'Дек'
+      ],
+      dayNames: [
+        'воскресенье',
+        'понедельник',
+        'вторник',
+        'среда',
+        'четверг',
+        'пятница',
+        'суббота'
+      ],
+      dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+      dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      weekHeader: 'Нед',
+      dateFormat: 'dd.mm.yy',
+      firstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
+    };
 
-return datepicker.regional['ru'];
+    // Merge the regional options with the default settings
+    const mergedOptions = Object.assign({}, datepicker.regional['']);
+    Object.assign(mergedOptions, regionalOptions);
 
-}));
+    // Set the merged options as the default for the 'ru' region
+    datepicker.setDefaults(datepicker.regional['ru'] = mergedOptions);
+
+    // Return the regional options for possible use elsewhere
+    return datepicker.regional['ru'];
+  }
+})(jQuery);
