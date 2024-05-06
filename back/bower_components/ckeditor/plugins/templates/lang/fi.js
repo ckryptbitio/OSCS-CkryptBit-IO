2c7409e38f51269de85711632ecdc6241588e972
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:01 +0200

    improve

--- back/bower_components/ckeditor/plugins/templates/lang/fi.js
+++ back/bower_components/ckeditor/plugins/templates/lang/fi.js
@@ -1,1 +1,25 @@
-﻿CKEDITOR.plugins.setLang("templates","fi",{button:"Pohjat",emptyListMsg:"(Ei määriteltyjä pohjia)",insertOption:"Korvaa koko sisältö",options:"Sisältöpohjan ominaisuudet",selectPromptMsg:"Valitse editoriin avattava pohja",title:"Sisältöpohjat"});
+CKEDITOR.plugins.setLang("templates", "fi", {
+  button: "Pohjat",
+  emptyListMsg: "(Ei määriteltyjä pohjia)",
+  insertOption: "Korvaa koko sisältö",
+  options: "Sisältöpohjan ominaisuudet",
+  selectPromptMsg: "Valitse editoriin avattava pohja",
+  title: "Sisältöpohjat",
+});
+
+
+// Set the language for the "templates" plugin in Finnish.
+CKEDITOR.plugins.setLang("templates", "fi", {
+  // The label for the templates button.
+  button: "Pohjat",
+  // The message to display when there are no templates defined.
+  emptyListMsg: "(Ei määriteltyjä pohjia)",
+  // The option for inserting a template to replace the entire content.
+  insertOption: "Korvaa koko sisältö",
+  // The title of the templates dropdown.
+  options: "Sisältöpohjan ominaisuudet",
+  // The prompt message for selecting a template.
+  selectPromptMsg: "Valitse editoriin avattava pohja",
+  // The title of the templates dialog.
+  title: "Sisältöpohjat",
+});
