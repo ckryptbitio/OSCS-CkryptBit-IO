3101e2a21d4d5e242e7eebfb0d89ba91bd8b2ffe
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:52 +0200

    improve

--- back/bower_components/ckeditor/plugins/iframe/lang/hi.js
+++ back/bower_components/ckeditor/plugins/iframe/lang/hi.js
@@ -1,1 +1,7 @@
-﻿CKEDITOR.plugins.setLang("iframe","hi",{border:"Show frame border",noUrl:"Please type the iframe URL",scrolling:"Enable scrollbars",title:"IFrame Properties",toolbar:"IFrame"});
+CKEDITOR.plugins.setLang("iframe", "hi", {
+  showFrameBorder: "दिखाएँ FRAME बॉर्डर",
+  enterIframeUrl: "कृपया IFrame URL टाइप करें",
+  enableScrollbars: "SCROLLBARS सक्षम करें",
+  iframePropertiesTitle: "IFrame गुणवत्ता",
+  iframeToolbarTitle: "IFrame",
+});
