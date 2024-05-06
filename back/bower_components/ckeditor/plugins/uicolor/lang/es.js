fb78641388c4cf4b131a80328990d68973233bca
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:58 +0200

    improve

--- back/bower_components/ckeditor/plugins/uicolor/lang/es.js
+++ back/bower_components/ckeditor/plugins/uicolor/lang/es.js
@@ -1,5 +1,20 @@
-﻿/*
- Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+/*
+Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("uicolor","es",{title:"Recolector de Color de Interfaz de Usuario",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"Conjuntos predefinidos de colores",config:"Pega esta cadena en tu archivo config.js"});
+
+// Set the language for the 'uicolor' plugin in Spanish
+CKEDITOR.plugins.setLang('uicolor', 'es', {
+  // Title of the dialog window
+  title: 'Recolector de Color de Interfaz de Usuario',
+  // Text for the option label
+  options: 'Color Options',
+  // Text for the highlight label
+  highlight: 'Highlight',
+  // Text for the selected color label
+  selected: 'Selected Color',
+  // Text for the predefined colors label
+  predefined: 'Conjuntos predefinidos de colores',
+  // Text for the config option label
+  config: 'Pega esta cadena en tu archivo config.js'
+});
