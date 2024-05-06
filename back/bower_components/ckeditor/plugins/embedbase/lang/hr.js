5cad99eb133d46c04294252664126e56f84dd476
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:57 +0200

    improve

--- back/bower_components/ckeditor/plugins/embedbase/lang/hr.js
+++ back/bower_components/ckeditor/plugins/embedbase/lang/hr.js
@@ -1,1 +1,11 @@
-﻿CKEDITOR.plugins.setLang("embedbase","hr",{pathName:"objekt medija",title:"Media Embed",button:"Umetanje Media Embed",unsupportedUrlGiven:"Navedeni URL nije podržan",unsupportedUrl:"URL {url} nije podržan od strane Media Embed-a.",fetchingFailedGiven:"Nije moguće dohvatiti sadržaj danog URL-a.",fetchingFailed:"Nije moguće dohvatiti sadržaj za {url}.",fetchingOne:"Dohvaćanje oEmbed odgovora...",fetchingMany:"Dohvaćanje oEmbed odgovora,  {current} od {max} gotovo..."});
+CKEDITOR.plugins.setLang("embedbase", "hr", {
+    pathName: "media objekt",
+    title: "Umetni Media Embed",
+    button: "Umetni Media Embed",
+    unsupportedUrlGiven: "Navedeni URL nije podržan.",
+    unsupportedUrl: "URL {url} nije podržan od strane Media Embed-a.",
+    fetchingFailedGiven: "Ne možemo pristupiti sadržaju na navedenom URL-u.",
+    fetchingFailed: "Ne možemo pristupiti sadržaju na URL-u {url}.",
+    fetchingOne: "Dohvaćanje oEmbed odgovora...",
+    fetchingMany: "Dohvaćanje oEmbed odgovora, {current} od {max} gotovo..."
+});
