29239e2b1ab8b3f5daac02c6f9485eb5ea6877ac
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:19 +0200

    improve

--- back/bower_components/ckeditor/plugins/iframe/lang/ar.js
+++ back/bower_components/ckeditor/plugins/iframe/lang/ar.js
@@ -1,1 +1,12 @@
-﻿CKEDITOR.plugins.setLang("iframe","ar",{border:"إظهار حدود الإطار",noUrl:"فضلا أكتب رابط الـ iframe",scrolling:"تفعيل أشرطة الإنتقال",title:"خصائص iframe",toolbar:"iframe"});
+CKEDITOR.plugins.add('iframe', {
+    lang: 'ar',
+    init: function(editor) {
+        editor.lang.iframe = {
+            border: 'إظهار حدود الإطار',
+            noUrl: 'فضلا أكتب رابط ال\-iframe',
+            scrolling: 'تفعيل أشرطة الإنتقال',
+            title: 'خصائص iframe',
+            toolbar: 'iframe'
+        };
+    }
+});
