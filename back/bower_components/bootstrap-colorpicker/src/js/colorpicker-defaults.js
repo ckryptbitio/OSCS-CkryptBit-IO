b55b89461cac8719c14e567e284fd1197bad698c
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:07 +0200

    improve

--- back/bower_components/bootstrap-colorpicker/src/js/colorpicker-defaults.js
+++ back/bower_components/bootstrap-colorpicker/src/js/colorpicker-defaults.js
@@ -1,7 +1,7 @@
 /*
  * Default plugin options
  */
-var defaults = {
+const defaults = {
   horizontal: false, // horizontal mode layout ?
   inline: false, //forces to show the colorpicker as an inline element
   color: false, //forces a color
@@ -52,13 +52,13 @@ var defaults = {
       callTop: false
     }
   },
-  template: '<div class="colorpicker dropdown-menu">' +
-    '<div class="colorpicker-saturation"><i><b></b></i></div>' +
-    '<div class="colorpicker-hue"><i></i></div>' +
-    '<div class="colorpicker-alpha"><i></i></div>' +
-    '<div class="colorpicker-color"><div /></div>' +
-    '<div class="colorpicker-selectors"></div>' +
-    '</div>',
+  template: `<div class="colorpicker dropdown-menu">
+    <div class="colorpicker-saturation"><i><b></b></i></div>
+    <div class="colorpicker-hue"><i></i></div>
+    <div class="colorpicker-alpha"><i></i></div>
+    <div class="colorpicker-color"><div /></div>
+    <div class="colorpicker-selectors"></div>
+  </div>`,
   align: 'right',
   customClass: null, // custom class added to the colorpicker element
   colorSelectors: null // custom color aliases
