77e8f90882aafab8515136563c7a3feabdad8610
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:57 +0200

    improve

--- back/bower_components/ckeditor/samples/old/assets/posteddata.php
+++ back/bower_components/ckeditor/samples/old/assets/posteddata.php
@@ -1,10 +1,4 @@
 <!DOCTYPE html>
-<?php
-/*
-Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-?>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -19,41 +13,22 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 		<colgroup><col width="120"></colgroup>
 		<thead>
 			<tr>
-				<th>Field&nbsp;Name</th>
-				<th>Value</th>
+				<th><?php echo htmlspecialchars('Field Name'); ?></th>
+				<th><?php echo htmlspecialchars('Value'); ?></th>
 			</tr>
 		</thead>
-<?php
+		<?php
+		error_reporting(E_ALL);
+		ini_set('display_errors', 1);
 
-if (!empty($_POST))
-{
-	foreach ( $_POST as $key => $value )
-	{
-		if ( ( !is_string($value) && !is_numeric($value) ) || !is_string($key) )
-			continue;
+		if ($_SERVER['REQUEST_METHOD'] === 'POST')
+		{
+			$data = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-		if ( get_magic_quotes_gpc() )
-			$value = htmlspecialchars( stripslashes((string)$value) );
-		else
-			$value = htmlspecialchars( (string)$value );
-?>
-		<tr>
-			<th style="vertical-align: top"><?php echo htmlspecialchars( (string)$key ); ?></th>
-			<td><pre class="samples"><?php echo $value; ?></pre></td>
-		</tr>
-	<?php
-	}
-}
-?>
-	</table>
-	<div id="footer">
-		<hr>
-		<p>
-			CKEditor - The text editor for the Internet - <a class="samples" href="https://ckeditor.com/">https://ckeditor.com</a>
-		</p>
-		<p id="copy">
-			Copyright &copy; 2003-2019, <a class="samples" href="https://cksource.com/">CKSource</a> - Frederico Knabben. All rights reserved.
-		</p>
-	</div>
-</body>
-</html>
+			if (!empty($data))
+			{
+				foreach ($data as $key => $value)
+				{
+					echo '<tr>';
+					echo '<th style="vertical-align: top">' . htmlspecialchars($key) . '</th>';
+		
