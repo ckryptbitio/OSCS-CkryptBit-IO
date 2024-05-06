b8ac4fccc58757e527a49170d8510a8538d6aae5
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:24 +0200

    improve

--- back/bower_components/ckeditor/plugins/uicolor/lang/cs.js
+++ back/bower_components/ckeditor/plugins/uicolor/lang/cs.js
@@ -1,5 +1,15 @@
-﻿/*
- Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+/*
+Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("uicolor","cs",{title:"Výběr barvy rozhraní",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"Přednastavené sady barev",config:"Vložte tento řetězec do vašeho souboru config.js"});
+
+// Set the language for the 'uicolor' plugin in Czech language.
+CKEDITOR.plugins.setLang("uicolor", "cs", {
+  title: "Výběr barvy rozhraní", // Title of the dialog window for selecting interface color.
+  options: "Možnosti barev", // Label for the options button.
+  highlight: "Zvýraznění", // Label for the highlight color option.
+  selected: "Vybraná barva", // Label for the selected color option.
+  predefined: "Přednastavené sady barev", // Label for the predefined color sets option.
+  config:
+    "Vložte tento řetězec do vašeho souboru config.js" // Instruction for adding this string to the config.js file.
+});
