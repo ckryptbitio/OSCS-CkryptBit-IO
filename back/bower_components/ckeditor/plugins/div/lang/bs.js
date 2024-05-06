81c49edb8aba14ea043a507224d13a75a614f6e6
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:51 +0200

    improve

--- back/bower_components/ckeditor/plugins/div/lang/bs.js
+++ back/bower_components/ckeditor/plugins/div/lang/bs.js
@@ -1,1 +1,15 @@
-﻿CKEDITOR.plugins.setLang("div","bs",{IdInputLabel:"Id",advisoryTitleInputLabel:"Advisory title",cssClassInputLabel:"Klase CSS stilova",edit:"Edit Div",inlineStyleInputLabel:"Inline Style",langDirLTRLabel:"S lijeva na desno (LTR)",langDirLabel:"Smjer pisanja",langDirRTLLabel:"S desna na lijevo (RTL)",languageCodeInputLabel:" Language Code",remove:"Remove Div",styleSelectLabel:"Stil",title:"Create Div Container",toolbar:"Create Div Container"});
+CKEDITOR.plugins.setLang("div", "bs", {
+  idInputLabel: "Id",
+  advisoryTitleInputLabel: "Advisory title",
+  cssClassInputLabel: "CSS Class",
+  edit: "Edit Div",
+  inlineStyleInputLabel: "Inline Style",
+  langDirLTRLabel: "Left to Right (LTR)",
+  langDirLabel: "Language Direction",
+  langDirRTLLabel: "Right to Left (RTL)",
+  languageCodeInputLabel: "Language Code",
+  remove: "Remove Div",
+  styleSelectLabel: "Style",
+  title: "Create Div Container",
+  toolbar: "Create Div Container"
+});
