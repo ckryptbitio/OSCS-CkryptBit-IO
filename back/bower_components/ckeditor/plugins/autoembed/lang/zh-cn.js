0bcae498f366913a79a0623d5e5ea16f1379bc0c
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:32 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/zh-cn.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/zh-cn.js
@@ -1,1 +1,11 @@
-﻿CKEDITOR.plugins.setLang("autoembed","zh-cn",{embeddingInProgress:"正在尝试嵌入粘贴的 URL 里的媒体内容...",embeddingFailed:"此 URL 无法自动嵌入媒体内容"});
+if (CKEDITOR.plugins.get("autoembed")) {
+  CKEDITOR.plugins.setLang(
+    "autoembed",
+    "zh-cn",
+    {
+      embeddingInProgress: "正在尝试嵌入粘贴的 URL 里的媒体内容...",
+      embeddingFailed: "此 URL 无法自动嵌入媒体内容",
+    },
+    true
+  );
+}
