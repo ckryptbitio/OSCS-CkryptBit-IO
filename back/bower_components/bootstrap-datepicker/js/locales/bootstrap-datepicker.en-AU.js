a0c286e7e1392404be25df60dd352198003a876d
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:58 +0200

    improve

--- back/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.en-AU.js
+++ back/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.en-AU.js
@@ -1,18 +1,21 @@
-/**
- * Australian English translation for bootstrap-datepicker
- * Steve Chapman <steven.p.chapman@gmail.com>
- */
-;(function($){
-	$.fn.datepicker.dates['en-AU'] = {
-		days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-		daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-		daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-		months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-		monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-		today: "Today",
-		monthsTitle: "Months",
-		clear: "Clear",
-		weekStart: 1,
-		format: "d/mm/yyyy"
-	};
+;(function($) {
+  // Australian English translation for bootstrap-datepicker
+  // Steve Chapman <steven.p.chapman@gmail.com>
+  // License: MIT
+
+  // Use template literals for readability
+  const AustralianEnglish = {
+    days: [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`],
+    daysShort: [`Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`],
+    daysMin: [`Su`, `Mo`, `Tu`, `We`, `Th`, `Fr`, `Sa`],
+    months: [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`],
+    monthsShort: [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dec`],
+    today: `Today`,
+    monthsTitle: `Months`,
+    clear: `Clear`,
+    weekStart: 1,
+    format: `d/mm/yyyy`
+  };
+
+  $.fn.datepicker.dates['en-AU'] = AustralianEnglish;
 }(jQuery));
