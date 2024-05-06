70ed497fea25efb229c6402e033c9a2fb5812d0a
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:49 +0200

    improve

--- back/bower_components/ckeditor/plugins/devtools/lang/sv.js
+++ back/bower_components/ckeditor/plugins/devtools/lang/sv.js
@@ -1,5 +1,26 @@
-﻿/*
+/*
  Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("devtools","sv",{title:"Elementinformation",dialogName:"Dialogrutans namn",tabName:"Fliknamn",elementId:"Element-ID",elementType:"Element-typ"});
+
+CKEDITOR.plugins.setLang("devtools", "sv", {
+  title: "Element information", // Dialog title
+  dialogName: "Dialog rutans namn",
+  tabName: "Fliknamn",
+  elementId: "Element ID",
+  elementType: "Element type"
+});
+
+
+/*
+ Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+
+CKEDITOR.plugins.setLang("devtools", "sv", {
+  title: "Element information", // Dialog title
+  dialogName: "Dialog rutans namn",
+  tabName: "Fliknamn",
+  elementId: "Element ID",
+  elementType: "Element type"
+});
