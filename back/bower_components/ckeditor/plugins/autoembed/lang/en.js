f0ee97b626ea06d364f3a3ddc17c33224b17ea78
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:27 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/en.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/en.js
@@ -1,1 +1,8 @@
-﻿CKEDITOR.plugins.setLang("autoembed","en",{embeddingInProgress:"Trying to embed pasted URL...",embeddingFailed:"This URL could not be automatically embedded."});
+CKEDITOR.plugins.setLang("autoembed", "en", {
+  embeddingInProgress: "Trying to embed the pasted URL...",
+  embeddingFailed_generic: "There was an error while embedding the URL. Please check the URL and try again.",
+  embeddingFailed_unsupportedProtocol: "The pasted URL uses an unsupported protocol, so it cannot be embedded automatically.",
+  embeddingFailed_accessDenied: "Access to the pasted URL has been denied, so it cannot be embedded automatically.",
+  embeddingFailed_timeout: "The request to embed the pasted URL has timed out, so it cannot be embedded automatically.",
+  embeddingFailed_other: "There was an unknown error while embedding the pasted URL. Please check the URL and try again."
+});
