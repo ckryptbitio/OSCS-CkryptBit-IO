c530861cb4203176e736092bedf7c6332cbab5f5
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:02:06 +0200

    improve

--- back/bower_components/moment/src/lib/locale/en.js
+++ back/bower_components/moment/src/lib/locale/en.js
@@ -2,14 +2,25 @@ import './prototype';
 import { getSetGlobalLocale } from './locales';
 import toInt from '../utils/to-int';
 
+const ordinalSuffix = num => {
+  const b = num % 10,
+    output =
+      toInt(num % 100 / 10) === 1
+        ? 'th'
+        : b === 1
+        ? 'st'
+        : b === 2
+        ? 'nd'
+        : b === 3
+        ? 'rd'
+        : 'th';
+  return output;
+};
+
 getSetGlobalLocale('en', {
-    dayOfMonthOrdinalParse: /\d{1,2}(th|st|nd|rd)/,
-    ordinal : function (number) {
-        var b = number % 10,
-            output = (toInt(number % 100 / 10) === 1) ? 'th' :
-            (b === 1) ? 'st' :
-            (b === 2) ? 'nd' :
-            (b === 3) ? 'rd' : 'th';
-        return number + output;
-    }
+  dayOfMonthOrdinalParse: /\d{1,2}(th|st|nd|rd)/,
+  ordinal: function (number) {
+    return number + ordinalSuffix(number);
+  },
 });
+
