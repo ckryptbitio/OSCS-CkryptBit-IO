0c4d9ad59ff29cd361fb80d1c30a96a8ad50375e
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:01 +0200

    improve

--- back/bower_components/ckeditor/plugins/colordialog/lang/fo.js
+++ back/bower_components/ckeditor/plugins/colordialog/lang/fo.js
@@ -1,1 +1,22 @@
-﻿CKEDITOR.plugins.setLang("colordialog","fo",{clear:"Strika",highlight:"Framheva",options:"Litmøguleikar",selected:"Valdur litur",title:"Vel lit"});
+CKEDITOR.plugins.setLang("colordialog", "fo", {
+  clear: "Strika",
+  highlight: "Framheva",
+  options: "Litmøguleikar",
+  selected: "Valdur litur",
+  title: "Vel lit",
+});
+
+
+// Set the language for the colordialog plugin in Faroese
+CKEDITOR.plugins.setLang("colordialog", "fo", {
+  // The text for the 'clear' button
+  clear: "Strika",
+  // The text for the 'highlight' button
+  highlight: "Framheva",
+  // The text for the 'options' menu
+  options: "Litmøguleikar",
+  // The text for the 'selected' color
+  selected: "Valdur litur",
+  // The title of the color dialog
+  title: "Vel lit",
+});
