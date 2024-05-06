5453c99174ea19617a3330aa65de2b0a9b10aab8
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:39 +0200

    improve

--- back/bower_components/ckeditor/plugins/embedbase/lang/bg.js
+++ back/bower_components/ckeditor/plugins/embedbase/lang/bg.js
@@ -1,1 +1,11 @@
-﻿CKEDITOR.plugins.setLang("embedbase","bg",{pathName:"медиен обект",title:"Вграждане на медия",button:"Вмъкни медия",unsupportedUrlGiven:"Посоченият URL адрес не се поддържа.",unsupportedUrl:"URL адресът {url} не се поддържа от вграждането на медия.",fetchingFailedGiven:"Извличането на съдържание за дадения URL адрес не е успешно.",fetchingFailed:"Извличането на съдържание за {url} не е успешно.",fetchingOne:"Извличане oEmbed заявка...",fetchingMany:"Извличане oEmbed заявка, {current} от {max}..."});
+CKEDITOR.plugins.setLang("embedbase", "bg", {
+    pathName: "медиен обект",
+    title: "Вграждане на медия",
+    button: "Вмъкни медия",
+    unsupportedUrlGiven: "Посоченият URL адрес не е поддържан.",
+    unsupportedUrl: "URL адресът {url} не е поддържан от вграждането на медия.",
+    fetchingFailedGiven: "Неуспешно извличане на съдържание за посочения URL адрес.",
+    fetchingFailed: "Неуспешно извличане на съдържание за {url}.",
+    fetchingOne: "Извличане на oEmbed заявка...",
+    fetchingMany: "Извличане на oEmbed заявка, {current} от {max}..."
+});
