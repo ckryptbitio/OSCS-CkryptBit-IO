57fcb9cbc24641d6d4e56b559fee13c9302626c4
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:51 +0200

    improve

--- back/bower_components/ckeditor/plugins/find/lang/ca.js
+++ back/bower_components/ckeditor/plugins/find/lang/ca.js
@@ -1,1 +1,15 @@
-﻿CKEDITOR.plugins.setLang("find","ca",{find:"Cerca",findOptions:"Opcions de Cerca",findWhat:"Cerca el:",matchCase:"Distingeix majúscules/minúscules",matchCyclic:"Coincidència cíclica",matchWord:"Només paraules completes",notFoundMsg:"El text especificat no s'ha trobat.",replace:"Reemplaça",replaceAll:"Reemplaça-ho tot",replaceSuccessMsg:"%1 ocurrència/es reemplaçada/es.",replaceWith:"Reemplaça amb:",title:"Cerca i reemplaça"});
+// Define the language object for Catalan (ca) for the 'find' plugin in CKEditor
+CKEDITOR.plugins.setLang('find', 'ca', {
+  find: 'Cerca',
+  findOptions: 'Opcions de cerca',
+  findWhat: 'Cerca el:',
+  matchCase: 'Distingeix majúscules i minúscules',
+  matchCyclic: 'Coincidència cíclica',
+  matchWord: 'Només paraules completes',
+  notFoundMsg: 'El text especificat no s\'ha trobat.',
+  replace: 'Reemplaça',
+  replaceAll: 'Reemplaça-ho tot',
+  replaceSuccessMsg: '%1 ocurrència/es reemplaçada/es.',
+  replaceWith: 'Reemplaça amb:',
+  title: 'Cerca i reemplaça'
+});
