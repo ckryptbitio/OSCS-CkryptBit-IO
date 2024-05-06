8f7be68cfc82a4d19ec9112c253e56ffcfbd320b
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:48 +0200

    improve

--- back/bower_components/ckeditor/plugins/colordialog/lang/ms.js
+++ back/bower_components/ckeditor/plugins/colordialog/lang/ms.js
@@ -1,1 +1,13 @@
-﻿CKEDITOR.plugins.setLang("colordialog","ms",{clear:"Clear",highlight:"Highlight",options:"Color Options",selected:"Selected Color",title:"Select color"});
+// Rewritten and improved code for CKEditor color dialog language configuration
+
+// Use const for constant variables
+const colorDialogLang = {
+  clear: "Bersih",
+  highlight: "Highlight",
+  options: "Pilihan Warna",
+  selected: "Warna Terpilih",
+  title: "Pilih Warna"
+};
+
+// Use CKEDITOR.plugins.addTranslations() to add translations for the 'colordialog' plugin
+CKEDITOR.plugins.addTranslations("colordialog", colorDialogLang, "ms");
