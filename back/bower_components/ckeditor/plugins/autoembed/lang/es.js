f09026142972589af9fe7b0be4db9225f2348f0f
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:53 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/es.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/es.js
@@ -1,1 +1,8 @@
-﻿CKEDITOR.plugins.setLang("autoembed","es",{embeddingInProgress:"Intentando incrustar URL pegada...",embeddingFailed:"Está URL no pudo ser automáticamente incrustada."});
+CKEDITOR.plugins.setLang("autoembed", "es", {
+  embeddingInProgress: "Tratando de incrustar URL copiada...",
+  embeddingFailed: "No fue posible incrustar automáticamente esta URL.",
+  // Add more error messages for better user experience
+  incorrectUrl: "La URL incrustada no es válida.",
+  unsupportedType: "El tipo de contenido incrustado no es compatible.",
+  exceededSize: "El tamaño del contenido incrustado excede el límite permitido.",
+});
