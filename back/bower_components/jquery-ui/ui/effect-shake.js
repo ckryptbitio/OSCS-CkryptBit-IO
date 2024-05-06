6bcb000fd9b46903cc70d1beecf24540caac676a
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:02:14 +0200

    improve

--- back/bower_components/jquery-ui/ui/effect-shake.js
+++ back/bower_components/jquery-ui/ui/effect-shake.js
@@ -8,77 +8,70 @@
  *
  * http://api.jqueryui.com/shake-effect/
  */
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
 
-		// AMD. Register as an anonymous module.
-		define([
-			"jquery",
-			"./effect"
-		], factory );
-	} else {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', './effect'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    const _ref = (direction) => (direction === 'up' || direction === 'down') ? 'top' : 'left';
+    const _positiveMotion = (direction) => (direction === 'up' || direction === 'left');
+    const _animationProperties = (distance, ref, positiveMotion) => ({
+        [ref]: positiveMotion ? `-=${distance}` : `+=${distance}`,
+    });
 
-		// Browser globals
-		factory( jQuery );
-	}
-}(function( $ ) {
+    const shakeEffect = (options, done) => {
+        const el = $(this);
+        const props = ['position', 'top', 'bottom', 'left', 'right', 'height', 'width'];
+        const mode = $.effects.setMode(el, options.mode || 'effect');
+        const direction = options.direction || 'left';
+        const distance = Math.abs(options.distance) || 20;
+        const times = options.times || 3;
+        const anims = times * 2 + 1;
+        const speed = Math.round(options.duration / anims);
+        const ref = _ref(direction);
+        const positiveMotion = _positiveMotion(direction);
+        const animation = _animationProperties(distance, ref, positiveMotion);
+        const animation1 = _animationProperties(distance * 2, ref, positiveMotion);
+        const animation2 = _animationProperties(distance * 2, ref, !positiveMotion);
 
-return $.effects.effect.shake = function( o, done ) {
+        $.effects.save(el, props);
+        el.show();
+        $.effects.createWrapper(el);
 
-	var el = $( this ),
-		props = [ "position", "top", "bottom", "left", "right", "height", "width" ],
-		mode = $.effects.setMode( el, o.mode || "effect" ),
-		direction = o.direction || "left",
-		distance = o.distance || 20,
-		times = o.times || 3,
-		anims = times * 2 + 1,
-		speed = Math.round( o.duration / anims ),
-		ref = (direction === "up" || direction === "down") ? "top" : "left",
-		positiveMotion = (direction === "up" || direction === "left"),
-		animation = {},
-		animation1 = {},
-		animation2 = {},
-		i,
+        el.animate(animation, speed, options.easing);
 
-		// we will need to re-assemble the queue to stack our animations in place
-		queue = el.queue(),
-		queuelen = queue.length;
+        for (let i = 1; i < times; i++) {
+            el.animate(animation1, speed, options.easing).animate(animation2, speed, options.easing);
+        }
 
-	$.effects.save( el, props );
-	el.show();
-	$.effects.createWrapper( el );
+        el
+            .animate(animation1, speed, options.easing)
+            .animate(animation, speed / 2, options.easing)
+            .queue(function () {
+                if (mode === 'hide') {
+                    el.hide();
+                }
+                $.effects.restore(el, props);
+                $.effects.removeWrapper(el);
+                done();
+            });
 
-	// Animation
-	animation[ ref ] = ( positiveMotion ? "-=" : "+=" ) + distance;
-	animation1[ ref ] = ( positiveMotion ? "+=" : "-=" ) + distance * 2;
-	animation2[ ref ] = ( positiveMotion ? "-=" : "+=" ) + distance * 2;
+        const queue = el.queue();
+        const queuelen = queue.length;
 
-	// Animate
-	el.animate( animation, speed, o.easing );
+        // inject all the animations we just queued to be first in line (after "inprogress")
+        if (queuelen > 1) {
+            queue.splice.apply(queue, [1, 0].concat(queue.splice(queuelen, anims + 1)));
+        }
+        el.dequeue();
+    };
 
-	// Shakes
-	for ( i = 1; i < times; i++ ) {
-		el.animate( animation1, speed, o.easing ).animate( animation2, speed, o.easing );
-	}
-	el
-		.animate( animation1, speed, o.easing )
-		.animate( animation, speed / 2, o.easing )
-		.queue(function() {
-			if ( mode === "hide" ) {
-				el.hide();
-			}
-			$.effects.restore( el, props );
-			$.effects.removeWrapper( el );
-			done();
-		});
-
-	// inject all the animations we just queued to be first in line (after "inprogress")
-	if ( queuelen > 1) {
-		queue.splice.apply( queue,
-			[ 1, 0 ].concat( queue.splice( queuelen, anims + 1 ) ) );
-	}
-	el.dequeue();
-
-};
-
+    $.effects.define('shake', {
+        shake: shakeEffect,
+    });
 }));
