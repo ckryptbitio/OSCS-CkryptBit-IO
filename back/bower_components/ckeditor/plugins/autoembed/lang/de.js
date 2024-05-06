964511b926ca8122b5169449ebba32f387225be1
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:22 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/de.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/de.js
@@ -1,1 +1,5 @@
-﻿CKEDITOR.plugins.setLang("autoembed","de",{embeddingInProgress:"Einbetten der eingefügten URL wird versucht...",embeddingFailed:"Diese URL konnte nicht automatisch eingebettet werden."});
+// Use the proper namespace for CKEditor 5
+CKEditor5.plugins.setLang('autoembed', {
+    embeddingInProgress: 'Das Einbetten der eingefügten URL wird versucht...',
+    embeddingFailed: 'Diese URL konnte nicht automatisch eingebettet werden.'
+});
