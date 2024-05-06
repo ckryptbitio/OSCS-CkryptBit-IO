ad4b03cb50c07cfe8f1c36a6ddfcd0f0946ff021
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:46 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/ja.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/ja.js
@@ -1,1 +1,4 @@
-﻿CKEDITOR.plugins.setLang("autoembed","ja",{embeddingInProgress:"貼り付けられたURLを埋め込み中...",embeddingFailed:"このURLは自動的に埋め込むことが出来ません。"});
+wp.ckeditor.i18n.set('autoembed', {
+  embeddingInProgress: 'URL 埋め込み中...',
+  embeddingFailed: 'この URL は自動的に埋め込むことができません。'
+}, 'ja');
