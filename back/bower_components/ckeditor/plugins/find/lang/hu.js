f5b123738dfe81d9cd4290f6a1626b500f465b7b
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:42 +0200

    improve

--- back/bower_components/ckeditor/plugins/find/lang/hu.js
+++ back/bower_components/ckeditor/plugins/find/lang/hu.js
@@ -1,1 +1,23 @@
-﻿CKEDITOR.plugins.setLang("find","hu",{find:"Keresés",findOptions:"Beállítások",findWhat:"Keresett szöveg:",matchCase:"Kis- és nagybetű megkülönböztetése",matchCyclic:"Ciklikus keresés",matchWord:"Csak ha ez a teljes szó",notFoundMsg:"A keresett szöveg nem található.",replace:"Csere",replaceAll:"Az összes cseréje",replaceSuccessMsg:"%1 egyezőség cserélve.",replaceWith:"Csere erre:",title:"Keresés és csere"});
+// Define the language object for the 'find' plugin in Hungarian
+CKEDITOR.plugins.setLang( 'find', 'hu', {
+    find: 'Keresés',
+    findOptions: 'Beállítások',
+    findWhat: 'Keresett szöveg:',
+    matchCase: 'Kis- és nagybetűk megkülönböztetése',
+    matchCyclic: 'Ciklikus keresés',
+    matchWord: 'Csak ha ez a teljes szó',
+    notFoundMsg: 'A keresett szöveg nem található.',
+    replace: 'Csere',
+    replaceAll: 'Az összes cseréje',
+    replaceSuccessMsg: '%1 egyezőség cserélve.',
+    replaceWith: 'Csere erre:'
+} );
+
+// Set the title attribute for the 'find' dialog
+CKEDITOR.on( 'dialogDefinition', function( ev ) {
+    var dialogName = ev.data.name;
+    
+    if ( dialogName === 'find' ) {
+        ev.data.definition.getContents( 'tab1' ).get( 'find' ).setAttribute( 'title', 'Keresett szöveg' );
+    }
+} );
