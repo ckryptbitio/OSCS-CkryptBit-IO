187ac749ee937cea96af997217a00c967de33387
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:46 +0200

    improve

--- back/bower_components/ckeditor/plugins/find/lang/fi.js
+++ back/bower_components/ckeditor/plugins/find/lang/fi.js
@@ -1,1 +1,18 @@
-﻿CKEDITOR.plugins.setLang("find","fi",{find:"Etsi",findOptions:"Hakuasetukset",findWhat:"Etsi mitä:",matchCase:"Sama kirjainkoko",matchCyclic:"Kierrä ympäri",matchWord:"Koko sana",notFoundMsg:"Etsittyä tekstiä ei löytynyt.",replace:"Korvaa",replaceAll:"Korvaa kaikki",replaceSuccessMsg:"%1 esiintymä(ä) korvattu.",replaceWith:"Korvaa tällä:",title:"Etsi ja korvaa"});
+// Define the language object for Finnish (fi) for the "find" plugin in CKEditor
+const find Finnish = {
+  find: "Etsi",
+  findOptions: "Hakuasetukset",
+  findWhat: "Etsi mitä:",
+  matchCase: "Sama kirjainkoko",
+  matchCyclic: "Kierrä ympäri",
+  matchWord: "Koko sana",
+  notFoundMsg: "Etsittyä tekstiä ei löytynyt.",
+  replace: "Korvaa",
+  replaceAll: "Korvaa kaikki",
+  replaceSuccessMsg: "%1 esiintymä(ä) korvattu.",
+  replaceWith: "Korvaa tällä:",
+  title: "Etsi ja korvaa"
+};
+
+// Set the language object for Finnish (fi) for the "find" plugin in CKEditor
+CKEDITOR.plugins.setLang("find", "fi", findFinnish);
