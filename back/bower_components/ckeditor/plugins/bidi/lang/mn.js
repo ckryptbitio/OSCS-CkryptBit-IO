3187e7249e5771495aefdf56abe925c86fe83496
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:34 +0200

    improve

--- back/bower_components/ckeditor/plugins/bidi/lang/mn.js
+++ back/bower_components/ckeditor/plugins/bidi/lang/mn.js
@@ -1,1 +1,8 @@
-﻿CKEDITOR.plugins.setLang("bidi","mn",{ltr:"Зүүнээс баруун тийш бичлэг",rtl:"Баруунаас зүүн тийш бичлэг"});
+// Define the language object for the 'bidi' plugin in Mongolian
+const bidiLangMn = {
+  ltr: 'Зүүнээс баруун тийш бичлэг',
+  rtl: 'Баруунаас зүүн тийш бичлэг'
+};
+
+// Register the language object with CKEditor
+CKEDITOR.plugins.setLang('bidi', 'mn', bidiLangMn);
