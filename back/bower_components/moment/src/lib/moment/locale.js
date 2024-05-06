ce1055cb185cba771e7ff063fd97eb7e25c74597
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:59 +0200

    improve

--- back/bower_components/moment/src/lib/moment/locale.js
+++ back/bower_components/moment/src/lib/moment/locale.js
@@ -1,34 +1,47 @@
-import { getLocale } from '../locale/locales';
+import { getLocale as getLocaleFromModule } from '../locale/locales';
 import { deprecate } from '../utils/deprecate';
 
-// If passed a locale key, it will set the locale for this
-// instance.  Otherwise, it will return the locale configuration
-// variables for this instance.
-export function locale (key) {
-    var newLocaleData;
+class Moment {
+  constructor() {
+    this._locale = {};
+  }
 
+  // Returns the current locale abbreviation
+  locale(key) {
     if (key === undefined) {
-        return this._locale._abbr;
-    } else {
-        newLocaleData = getLocale(key);
-        if (newLocaleData != null) {
-            this._locale = newLocaleData;
-        }
-        return this;
+      return this._locale._abbr;
     }
-}
 
-export var lang = deprecate(
-    'moment().lang() is deprecated. Instead, use moment().localeData() to get the language configuration. Use moment().locale() to change languages.',
-    function (key) {
-        if (key === undefined) {
-            return this.localeData();
-        } else {
-            return this.locale(key);
-        }
+    const newLocaleData = getLocaleFromModule(key);
+
+    if (newLocaleData) {
+      this._locale = newLocaleData;
     }
-);
 
-export function localeData () {
+    return this;
+  }
+
+  // Deprecated. Use locale() instead.
+  lang(key) {
+    deprecate(
+      'moment().lang() is deprecated. Instead, use moment().localeData() to get the language configuration. Use moment().locale() to change languages.',
+      function (...args) {
+        if (args.length === 0) {
+          return this.localeData();
+        }
+
+        return this.locale(key);
+      }
+    ).apply(this, arguments);
+  }
+
+  // Returns the language configuration
+  localeData() {
     return this._locale;
+  }
 }
+
+// Overwrite the original exported functions with our class instance methods
+export const locale = (key) => new Moment().locale(key);
+export const lang = (key) => new Moment().lang(key);
+export const localeData = () => new Moment().localeData();
