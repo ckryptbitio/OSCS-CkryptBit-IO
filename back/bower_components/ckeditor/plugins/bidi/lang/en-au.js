2cd7db6a449391700467d2363cff6cd86936a57a
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:29 +0200

    improve

--- back/bower_components/ckeditor/plugins/bidi/lang/en-au.js
+++ back/bower_components/ckeditor/plugins/bidi/lang/en-au.js
@@ -1,1 +1,4 @@
-﻿CKEDITOR.plugins.setLang("bidi","en-au",{ltr:"Text direction from left to right",rtl:"Text direction from right to left"});
+CKEDITOR.plugins.setLang( 'bidi', 'en', {
+  ltr: 'Text direction from left to right',
+  rtl: 'Text direction from right to left'
+} );
