edfa0049fa6d47154c86d04c81c2d188f687e79f
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:24 +0200

    improve

--- back/bower_components/ckeditor/plugins/autoembed/lang/it.js
+++ back/bower_components/ckeditor/plugins/autoembed/lang/it.js
@@ -1,1 +1,12 @@
-﻿CKEDITOR.plugins.setLang("autoembed","it",{embeddingInProgress:"Prova a incorporare l'URL incollato...",embeddingFailed:"Non è stato possibile incorporare automaticamente questo URL."});
+// Use const instead of var for constant variables
+const langConfig = {
+  "autoembed": {
+    "it": {
+      "embeddingInProgress": "Provando ad incorporare l'URL incollato...",
+      "embeddingFailed": "Impossibile incorporare automaticamente questo URL."
+    }
+  }
+};
+
+// Use CKEDITOR.plugins.addTranslations instead of CKEDITOR.plugins.setLang
+CKEDITOR.plugins.addTranslations(langConfig);
