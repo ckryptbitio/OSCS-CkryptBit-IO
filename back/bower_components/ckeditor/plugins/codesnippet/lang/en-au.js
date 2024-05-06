653cab95f05a6cdba4dd82c4d55cd80cea7195ea
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:44 +0200

    improve

--- back/bower_components/ckeditor/plugins/codesnippet/lang/en-au.js
+++ back/bower_components/ckeditor/plugins/codesnippet/lang/en-au.js
@@ -1,5 +1,17 @@
-﻿/*
+/*
  Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("codesnippet","en-au",{button:"Insert Code Snippet",codeContents:"Code content",emptySnippetError:"A code snippet cannot be empty.",language:"Language",title:"Code snippet",pathName:"code snippet"});
+
+// Use const instead of var for constant variables
+const pluginName = 'codesnippet';
+const langCode = 'en-au';
+
+CKEDITOR.plugins.setLang(pluginName, langCode, {
+  button: 'Insert Code Snippet',
+  codeContents: 'Code content',
+  emptySnippetError: 'A code snippet cannot be empty.',
+  language: 'Language',
+  title: 'Code snippet',
+  pathName: 'code snippet'
+});
