7b35727da0deba3206ac3c2d58158204b46f9950
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:03 +0200

    improve

--- back/bower_components/ckeditor/plugins/bidi/lang/da.js
+++ back/bower_components/ckeditor/plugins/bidi/lang/da.js
@@ -1,1 +1,8 @@
-﻿CKEDITOR.plugins.setLang("bidi","da",{ltr:"Tekstretning fra venstre til højre",rtl:"Tekstretning fra højre til venstre"});
+// Define the language object for Bidi plugin in Danish
+const bidiLangDa = {
+  ltr: 'Tekstretning fra venstre til højre',
+  rtl: 'Tekstretning fra højre til venstre'
+};
+
+// Assign the language object to the CKEditor's Bidi plugin
+CKEDITOR.plugins.setLang('bidi', 'da', bidiLangDa);
