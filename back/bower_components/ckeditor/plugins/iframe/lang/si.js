708d277cbc316935f598faa4359de146a0cb06b9
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:05 +0200

    improve

--- back/bower_components/ckeditor/plugins/iframe/lang/si.js
+++ back/bower_components/ckeditor/plugins/iframe/lang/si.js
@@ -1,1 +1,7 @@
-﻿CKEDITOR.plugins.setLang("iframe","si",{border:"සැකිල්ලේ කඩයිම් ",noUrl:"කරුණාකර රුපයේ URL ලියන්න",scrolling:"සක්ක්‍රිය කරන්න",title:"IFrame Properties",toolbar:"IFrame"});
+CKEDITOR.plugins.setLang("iframe", "si", {
+    border: "කඩයිම් පරිදිබීම",
+    noUrl: "URL �������� givenn",
+    scrolling: "බොහෝ ගමන",
+    title: "IFrame enschaften",
+    toolbar: "IFrame"
+});
