dd14a4dd2ed38550c51fcf0024f27b98d9744353
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:02:07 +0200

    improve

--- back/bower_components/inputmask/dist/inputmask/jquery.inputmask.js
+++ back/bower_components/inputmask/dist/inputmask/jquery.inputmask.js
@@ -1,62 +1,87 @@
-/*!
-* jquery.inputmask.js
-* https://github.com/RobinHerbots/Inputmask
-* Copyright (c) 2010 - 2017 Robin Herbots
-* Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 3.3.11
-*/
+(function($, Inputmask) {
+  'use strict';
 
-!function(factory) {
-    "function" == typeof define && define.amd ? define([ "jquery", "./inputmask" ], factory) : "object" == typeof exports ? module.exports = factory(require("jquery"), require("./inputmask")) : factory(jQuery, window.Inputmask);
-}(function($, Inputmask) {
-    return void 0 === $.fn.inputmask && ($.fn.inputmask = function(fn, options) {
-        var nptmask, input = this[0];
-        if (void 0 === options && (options = {}), "string" == typeof fn) switch (fn) {
-          case "unmaskedvalue":
-            return input && input.inputmask ? input.inputmask.unmaskedvalue() : $(input).val();
+  $.fn.inputmask = function(fn, options) {
+    const input = this[0];
 
-          case "remove":
+    if (typeof options === 'undefined') {
+      options = {};
+    }
+
+    if (typeof fn === 'string') {
+      switch (fn) {
+        case 'unmaskedvalue':
+          return input && input.inputmask ? input.inputmask.unmaskedvalue() : $(input).val();
+
+        case 'remove':
+          return this.each(function() {
+            if (this.inputmask) {
+              this.inputmask.remove();
+            }
+          });
+
+        case 'getemptymask':
+          return input && input.inputmask ? input.inputmask.getemptymask() : '';
+
+        case 'hasMaskedValue':
+          return !(!input || !input.inputmask) && input.inputmask.hasMaskedValue();
+
+        case 'isComplete':
+          return !input || !input.inputmask || input.inputmask.isComplete();
+
+        case 'getmetadata':
+          return input && input.inputmask ? input.inputmask.getmetadata() : undefined;
+
+        case 'setvalue':
+          $(input).val(options);
+          if (input && typeof input.inputmask === 'undefined') {
+            $(input).triggerHandler('setvalue');
+          }
+          break;
+
+        case 'option':
+          if (typeof options === 'string') {
             return this.each(function() {
-                this.inputmask && this.inputmask.remove();
+              if (typeof this.inputmask !== 'undefined') {
+                this.inputmask.option(options);
+              }
             });
+          }
+          if (input && typeof input.inputmask !== 'undefined') {
+            input.inputmask.option(options);
+          }
+          break;
 
-          case "getemptymask":
-            return input && input.inputmask ? input.inputmask.getemptymask() : "";
-
-          case "hasMaskedValue":
-            return !(!input || !input.inputmask) && input.inputmask.hasMaskedValue();
-
-          case "isComplete":
-            return !input || !input.inputmask || input.inputmask.isComplete();
-
-          case "getmetadata":
-            return input && input.inputmask ? input.inputmask.getmetadata() : void 0;
-
-          case "setvalue":
-            $(input).val(options), input && void 0 === input.inputmask && $(input).triggerHandler("setvalue");
-            break;
-
-          case "option":
-            if ("string" != typeof options) return this.each(function() {
-                if (void 0 !== this.inputmask) return this.inputmask.option(options);
-            });
-            if (input && void 0 !== input.inputmask) return input.inputmask.option(options);
-            break;
-
-          default:
-            return options.alias = fn, nptmask = new Inputmask(options), this.each(function() {
-                nptmask.mask(this);
-            });
-        } else {
-            if ("object" == typeof fn) return nptmask = new Inputmask(fn), void 0 === fn.mask && void 0 === fn.alias ? this.each(function() {
-                if (void 0 !== this.inputmask) return this.inputmask.option(fn);
-                nptmask.mask(this);
-            }) : this.each(function() {
-                nptmask.mask(this);
-            });
-            if (void 0 === fn) return this.each(function() {
-                (nptmask = new Inputmask(options)).mask(this);
-            });
+        default:
+          options.alias = fn;
+          const nptmask = new Inputmask(options);
+          return this.each(function() {
+            nptmask.mask(this);
+          });
+      }
+    } else {
+      if (typeof fn === 'object') {
+        const nptmask = new Inputmask(fn);
+        if (typeof fn.mask === 'undefined' && typeof fn.alias === 'undefined') {
+          return this.each(function() {
+            if (typeof this.inputmask !== 'undefined') {
+              return this.inputmask.option(fn);
+            }
+            nptmask.mask(this);
+          });
         }
-    }), $.fn.inputmask;
-});
+        return this.each(function() {
+          nptmask.mask(this);
+        });
+      }
+      if (typeof fn === 'undefined') {
+        return this.each(function() {
+          const nptmask = new Inputmask(options);
+          nptmask.mask(this);
+        });
+      }
+    }
+
+    return $.fn.inputmask;
+  };
+})(jQuery, window.Inputmask);
