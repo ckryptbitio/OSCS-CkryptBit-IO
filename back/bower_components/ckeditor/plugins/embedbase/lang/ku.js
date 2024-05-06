912d287ba835aee84d482428dcbb4f73e571c51c
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:59 +0200

    improve

--- back/bower_components/ckeditor/plugins/embedbase/lang/ku.js
+++ back/bower_components/ckeditor/plugins/embedbase/lang/ku.js
@@ -1,1 +1,11 @@
-﻿CKEDITOR.plugins.setLang("embedbase","ku",{pathName:"ڕاگەیاندنی بەرکار",title:"خستنەناوی ڕاگەیاندن",button:"خستنەناوی ڕاگەیاندن",unsupportedUrlGiven:"بەستەری نیشانکراو پشتیوان نەکراوە.",unsupportedUrl:"بەستەری {url} پشتیواننەککراوە لەلایەن خستنەناوی ڕاگەیاندن.",fetchingFailedGiven:"سەرکەوتوونەبوو لە هێنانی ناوەڕۆکی بەستەری دراو",fetchingFailed:"سەرکەوتوونەبوو لە هێنانەی ناوەڕۆکی ئەم بەستەرە {url}.",fetchingOne:"لە هەوڵی وەڵامی خستنەناوە",fetchingMany:"لە هەوڵی هێنانی خستنەناوە, {current} لە {max} کۆتایهاتووە..."});
+CKEDITOR.plugins.setLang("embedbase", "ku", {
+    pathName: "ڕاگەیاندنی بەرکار",
+    title: "خستنەناوی ڕاگەیاندن",
+    button: "خستنەناوی ڕاگەیاندن",
+    unsupportedUrlGiven: "ناپشتیوانی بەستەری بە دروستکردن نەکراوە.",
+    unsupportedUrl: "بەستەری '{url}' پشتیواننەکراوە بۆ خستنەناوی ڕاگەیاندن.",
+    fetchingFailedGiven: "دراو نەبوو لە هێنانی ناوەڕۆکی بەستەری دراو",
+    fetchingFailed: "دراو نەبوو لە هێنانەی ناوەڕۆکی بەستەری {url}.",
+    fetchingOne: "در وەڵامی خستنەناوە",
+    fetchingMany: "در هێنانی خستنەناوە, {current} لە {max} کۆتایەکان ..."
+});
