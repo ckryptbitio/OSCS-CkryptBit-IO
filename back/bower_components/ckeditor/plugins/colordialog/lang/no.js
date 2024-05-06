86587bc5c110c68d53d187fae8fd60c73d74b2ce
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:55 +0200

    improve

--- back/bower_components/ckeditor/plugins/colordialog/lang/no.js
+++ back/bower_components/ckeditor/plugins/colordialog/lang/no.js
@@ -1,1 +1,22 @@
-﻿CKEDITOR.plugins.setLang("colordialog","no",{clear:"Tøm",highlight:"Merk",options:"Alternativer for farge",selected:"Valgt",title:"Velg farge"});
+CKEDITOR.plugins.setLang("colordialog", "no", {
+  clear: "Tøm",
+  highlight: "Merk",
+  options: "Alternativer for farge",
+  selected: "Valgt",
+  title: "Velg farge",
+});
+
+
+// Set the language for the colordialog plugin in Norwegian
+CKEDITOR.plugins.setLang("colordialog", "no", {
+  // The label for the "clear" button
+  clear: "Tøm",
+  // The label for the "highlight" button
+  highlight: "Merk",
+  // The label for the dropdown with color options
+  options: "Alternativer for farge",
+  // The label for the currently selected color
+  selected: "Valgt",
+  // The title of the color picker dialog
+  title: "Velg farge",
+});
