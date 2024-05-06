56c410fd1d8f21efde16b0d5fe75f8c7e54a3381
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:51 +0200

    improve

--- back/bower_components/ckeditor/plugins/devtools/lang/uk.js
+++ back/bower_components/ckeditor/plugins/devtools/lang/uk.js
@@ -1,5 +1,21 @@
-﻿/*
+/*
  Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("devtools","uk",{title:"Відомості про Елемент",dialogName:"Заголовок діалогового вікна",tabName:"Назва вкладки",elementId:"Ідентифікатор Елемента",elementType:"Тип Елемента"});
+
+CKEDITOR.plugins.setLang("devtools", "uk", {
+  title: "Елемент Details", // changed to "Details" for better readability
+  dialogName: "Діалогове Вікно Заголовок", // changed to "Dialog Window Title" for better readability
+  tabName: "Вкладка Назва", // changed to "Tab Name" for better readability
+  elementId: "Елемент ID", // changed to "Element ID" for better readability
+  elementType: "Тип Елемента" // no changes made
+});
+
+
+CKEDITOR.plugins.setLang("devtools", "uk", {
+  title: "Елемент Details",
+  dialogName: "Діалогове Вікно Заголовок",
+  tabName: "Вкладка Назва",
+  elementId: "Елемент ID",
+  elementType: "Тип Елемента"
+});
