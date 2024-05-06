9dffadc52f666e7e22fa3b56a4298d65d6304326
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:00:27 +0200

    improve

--- back/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.sr-latin.js
+++ back/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.sr-latin.js
@@ -2,15 +2,21 @@
  * Serbian latin translation for bootstrap-datepicker
  * Bojan Milosavlević <milboj@gmail.com>
  */
-;(function($){
-	$.fn.datepicker.dates['sr-latin'] = {
-		days: ["Nedelja","Ponedeljak", "Utorak", "Sreda", "Četvrtak", "Petak", "Subota"],
-		daysShort: ["Ned", "Pon", "Uto", "Sre", "Čet", "Pet", "Sub"],
-		daysMin: ["N", "Po", "U", "Sr", "Č", "Pe", "Su"],
-		months: ["Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar"],
-		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"],
-		today: "Danas",
-		weekStart: 1,
-		format: "dd.mm.yyyy"
-	};
+(function($) {
+  'use strict';
+
+  $.fn.datepicker.dates['sr-latin'] = {
+    days: ["Nedelja", "Ponedeljak", "Utorak", "Sreda", "Četvrtak", "Petak", "Subota"],
+    daysShort: ["Ned", "Pon", "Uto", "Sre", "Čet", "Pet", "Sub"],
+    daysMin: ["N", "Po", "Ut", " Sr", "Če", "Pe", "Su"],
+    months: ["Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"],
+    today: "Danas",
+    weekStart: 1,
+    format: "dd.mm.yyyy",
+    clear: "Obriši",
+    close: "Zatvori",
+    dateFormat: "dd.mm.yyyy",
+    firstDay: 1,
+  };
 }(jQuery));
