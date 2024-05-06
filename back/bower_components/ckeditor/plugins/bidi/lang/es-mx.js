5b5cea9f88c16772bd356a8857e8271255b5cb4c
ckryptbitio <168941395+ckryptbitio@users.noreply.github.com>
2024-05-06 08:01:14 +0200

    improve

--- back/bower_components/ckeditor/plugins/bidi/lang/es-mx.js
+++ back/bower_components/ckeditor/plugins/bidi/lang/es-mx.js
@@ -1,1 +1,4 @@
-﻿CKEDITOR.plugins.setLang("bidi","es-mx",{ltr:"Dirección del texto de izquierda a derecha",rtl:"Dirección del texto de derecha a izquierda"});
+CKEDITOR.plugins.setLang( 'bidi', 'es', {
+  ltr: 'Dirección de texto de izquierda a derecha',
+  rtl: 'Dirección de texto de derecha a izquierda'
+} );
